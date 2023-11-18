12de4ac24ba2d5126874942b6d7949deb60d4e16
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-11-18 11:58:52 -0700

    advancing layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,11 @@ import { useRef } from "react"
 import { setTrainerName } from "../store/slices/trainerName.slice.js"
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import './styles/HomePage.css'
+import Pokedexlogo from '../assets/img/poketitle.png'
+import Pokeballsup from '../assets/img/pokeball.png'
+import Pokeballinf from '../assets/img/pokeballbottom.png'
+import Tablero from '../assets/img/tablero.png'
 
 
 const HomePage = () => {
@@ -20,15 +25,20 @@ const HomePage = () => {
     }
 
   return (
-    <div>
-        <h1>Pokedex</h1>
-        <h2>Hi Trainer!</h2>
-        <p>To start, please give me your trainer name</p>
-        <form onSubmit={handleSubmit}>
-            <input ref={inputName} type="text" />
-            <button>Catch them all!</button>
-        </form>
-    </div>
+    <main className='home'>
+      <div className='container'>
+          <img className='home__logo' src={Pokedexlogo} alt='Pokedex Logo' />
+          
+          <div className='pokeball'>
+            <img className="pokesup" src={Pokeballsup} alt="Poke Tapa" />
+            <img className="pokeinf" src={Pokeballinf} alt="Poke Tapa" />
+          </div>
+          <form onSubmit={handleSubmit}>
+              <input ref={inputName} type="text" />
+              <button>Catch them all!</button>
+          </form>
+      </div>
+    </main>
   )
 }
 
