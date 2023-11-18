7d5ed4c46d9ab6966799cb2820097f946bbb9b83
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-11-18 13:19:21 -0700

    pokeinfopage

--- src/pages/PokeInfoPage.jsx
+++ src/pages/PokeInfoPage.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+import { useParams } from "react-router-dom"
+import useFetch from "../hooks/useFetch";
+import { useEffect } from "react";
 
 const PokeInfoPage = () => {
 
-  const { id } = useParams()
+  const { id } = useParams();
 
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`
   const [ pokemon, getPokemon ] = useFetch(url)
 
-  useEffect(() => {
+  useEffect( () => {
     getPokemon()
-  }, [])
+  },[])
+
+  console.log(pokemon);
 
   return (
     <div>
-      <h1>PokeInfoPage</h1>
+      <h1>SetInfoPage</h1>
+      <img src={pokemon?.sprites.other["official-artwork"].front_default} alt=""></img>
     </div>
   )
 }
