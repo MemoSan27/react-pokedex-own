a4ce0fed2a6663a62179fb24a6b9934af026c308
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-11-19 15:47:13 -0700

    creating helpers

--- src/components/PokedexPage/PokeCard.jsx
+++ src/components/PokedexPage/PokeCard.jsx
@@ -3,6 +3,7 @@ import useFetch from "../../hooks/useFetch"
 import { useNavigate } from "react-router-dom"
 import './styles/PokeCard.css'
 import Typewriter from 'typewriter-effect';
+import { getBackground, getColor } from "../../helpers/helper";
 
 const PokeCard = ({ url }) => {
 
@@ -18,131 +19,12 @@ const PokeCard = ({ url }) => {
     navigate(`/pokedex/${infoPoke.id}`)
   }
 
-  const getBackground = (index = 0) => {
-    let type = infoPoke?.types?.[index].type.name;
-    switch (type) {
-        case "normal":
-            return "#b19176"
-            break;
-        case "fighting":
-            return "#c03636"
-            break;
-        case "flying":
-            return "#568aa8"
-            break;
-        case "poison":
-            return "#964bc2"
-            break;
-        case "ground":
-            return "#722f03"
-            break;
-        case "rock":
-            return "#81404b"
-            break;
-        case "bug":
-            return "#0fccb3"
-            break;
-        case "ghost":
-            return "#cd40f8"
-            break;
-        case "steel":
-            return "#d6d3d3"
-            break;
-        case "fire":
-            return "#F8B78B"
-            break;
-        case "water":
-            return "#2697f3"
-            break;
-        case "grass":
-            return "greenyellow"
-            break;
-        case "electric":
-            return "#caaf36"
-            break;
-        case "psychic":
-            return "#fa7e93"
-            break;
-        case "ice":
-            return "#b9e3f3"
-            break;
-        case "dragon":
-            return "#fa4801"
-            break;
-        case "dark":
-            return "gray"
-            break;
-        case "fairy":
-            return "pink"
-            break
-    }
-}
-
-const getColor = (index = 0) => {
-  let type = infoPoke?.types?.[index].type.name;
-  switch (type) {
-      case "normal":
-          return "rgb(255,255,255)"
-          break;
-      case "fighting":
-          return "rgb(255,255,255)"
-          break;
-      case "flying":
-          return "rgb(255,255,255)"
-          break;
-      case "poison":
-          return "rgb(255,255,255)"
-          break;
-      case "ground":
-          return "rgb(255,255,255)"
-          break;
-      case "rock":
-          return "rgb(255,255,255)"
-          break;
-      case "bug":
-          return "rgb(255,255,255)"
-          break;
-      case "ghost":
-          return "rgb(255,255,255)"
-          break;
-      case "steel":
-          return "rgb(0,0,0)"
-          break;
-      case "fire":
-          return "rgb(255,255,255)"
-          break;
-      case "water":
-          return "rgb(255,255,255)"
-          break;
-      case "grass":
-          return "rgb(105,105,105)"
-          break;
-      case "electric":
-          return "rgb(255,255,255)"
-          break;
-      case "psychic":
-          return "rgb(255,255,255)"
-          break;
-      case "ice":
-          return "rgb(0,0,0)"
-          break;
-      case "dragon":
-          return "rgb(255,255,255)"
-          break;
-      case "dark":
-          return "rgb(255,255,255)"
-          break;
-      case "fairy":
-          return "rgb(0,0,0)"
-          break
-  }
-}
-
   
+
   return (
    <article className="card" onClick={handleNavigate}>
-    <header style={{ backgroundColor: getBackground() }} className="header">
-      <h3 style={{ color: getColor() }} className="card__poke-name"> 
+    <header style={{ backgroundColor: getBackground(0, infoPoke) }} className="header">
+      <h3 style={{ color: getColor(0, infoPoke) }} className="card__poke-name"> 
         <Typewriter
                               options={{
                                   strings: [  `${infoPoke?.name}`, 
@@ -162,8 +44,8 @@ const getColor = (index = 0) => {
         {
           infoPoke?.types.map((infoType, index) => (
             <li key={infoType.type.url}>
-              <div style={{ backgroundColor: getBackground(index) }} className="type__box">
-                <p style={{ color: getColor(index) }} className="type__name">{infoType.type.name.toUpperCase()}</p>
+              <div style={{ backgroundColor: getBackground(index, infoPoke) }} className="type__box">
+                <p style={{ color: getColor(index, infoPoke) }} className="type__name">{infoType.type.name.toUpperCase()}</p>
               </div>
             </li>
           ))
@@ -174,15 +56,15 @@ const getColor = (index = 0) => {
         {
           infoPoke?.stats.map(infoStat => (
             <li key={infoStat.stat.url}>
-              <span style={{ color: getColor() }} className="card__span span__name">{infoStat.stat.name}</span>
-              <span style={{ color: getColor() }} className="card__span span__stat">{infoStat.base_stat}</span>
+              <span style={{ color: getColor(0, infoPoke) }} className="card__span span__name">{infoStat.stat.name}</span>
+              <span style={{ color: getColor(0, infoPoke) }} className="card__span span__stat">{infoStat.base_stat}</span>
               { console.log(infoStat) }
             </li>
           ))
         }
       </ul>
     </section>
-    <div style={{ backgroundColor: getBackground() }} className="card__footer">
+    <div style={{ backgroundColor: getBackground(0, infoPoke) }} className="card__footer">
 
     </div>
    </article>
--- src/pages/PokeInfoPage.jsx
+++ src/pages/PokeInfoPage.jsx
@@ -2,13 +2,16 @@ import { useParams } from "react-router-dom"
 import useFetch from "../hooks/useFetch";
 import { useEffect } from "react";
 import './styles/PokeInfo.css'
+import { getBackground } from "../helpers/helper";
 
 const PokeInfoPage = () => {
 
   const { id } = useParams();
 
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-  const [ pokemon, getPokemon ] = useFetch(url)
+  const [ pokemon, getPokemon ] = useFetch(url);
+
+  
 
   useEffect( () => {
     getPokemon()
@@ -17,7 +20,7 @@ const PokeInfoPage = () => {
   console.log(pokemon);
 
   return (
-    <div className="poke-info__container">
+    <div style={{ backgroundColor: getBackground(0, pokemon) }} className="poke-info__container">
       <div className="poke-info__img">
         <img src={pokemon?.sprites.other["official-artwork"].front_default} alt=""></img>
       </div>
