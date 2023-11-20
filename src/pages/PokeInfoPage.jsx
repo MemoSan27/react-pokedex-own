a1f81b27a18040fe186e77063986bb83a4cb8dab
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-11-19 21:23:41 -0700

    skills and moves added

--- src/pages/PokeInfoPage.jsx
+++ src/pages/PokeInfoPage.jsx
@@ -27,54 +27,96 @@ const PokeInfoPage = () => {
 }
 
   return (
-    <div style={{ backgroundColor: getBackground(0, pokemon)}} className="poke-info__container">
-      <div className="poke-info__img">
-        <h1 style={{ color: getColor(0, pokemon) }} className="poke-info__name"> 
-          <Typewriter
-                                options={{
-                                    strings: [  `${pokemon?.name}`, 
-                                                ],
-                                    autoStart: true,
-                                    loop: true,
-                                }}
-                            />
-        </h1>
-        <div className="poke-info__height">
-          <img className='avatar' src={getImage()} alt="" />
-          <p style={{ color: getColor(0, pokemon) }} className="poke-info__desc"> Height:</p>                      
-          <p style={{ color: getColor(0, pokemon) }} className="poke-info__info"> {pokemon?.height} "</p>                      
-          <p style={{ color: getColor(0, pokemon) }} className="poke-info__desc"> Weight: </p>                      
-          <p style={{ color: getColor(0, pokemon) }} className="poke-info__info"> {pokemon?.weight} pounds </p>                      
-        </div>
-        <img className="poke-info__pic" src={pokemon?.sprites.other["official-artwork"].front_default} alt=""></img>
-        <div className="cajas">
-          <div className="type__container">
-            <p className="type__legend"> Type </p>
-            <div className="type__container-inside">
-            {
-            pokemon?.types.map((infoType, index) => (
-              <div key={infoType.type.name} style={{ backgroundColor: getBackground(index, pokemon) }} className="type__box">
-                <p  style={{ color: getColor(index, pokemon) }} className="type__name">{infoType.type.name.toUpperCase()}</p>
+    <section className="pokeinfo">
+      <main style={{ backgroundColor: getBackground(0, pokemon)}} className="poke-info__container">
+        <div className="poke-info__img">
+          <h1 style={{ color: getColor(0, pokemon) }} className="poke-info__name"> 
+            <Typewriter
+                                  options={{
+                                      strings: [  `${pokemon?.name}`, 
+                                                  ],
+                                      autoStart: true,
+                                      loop: true,
+                                  }}
+                              />
+          </h1>
+          <div className="poke-info__height">
+            <img className='avatar' src={getImage()} alt="" />
+            <p style={{ color: getColor(0, pokemon) }} className="poke-info__desc"> Height:</p>                      
+            <p style={{ color: getColor(0, pokemon) }} className="poke-info__info"> {pokemon?.height} "</p>                      
+            <p style={{ color: getColor(0, pokemon) }} className="poke-info__desc"> Weight: </p>                      
+            <p style={{ color: getColor(0, pokemon) }} className="poke-info__info"> {pokemon?.weight} pounds </p>                      
+          </div>
+          <img className="poke-info__pic" src={pokemon?.sprites.other["official-artwork"].front_default} alt=""></img>
+          <div className="cajas">
+            <div className="type__container">
+              <p className="type__legend"> Type </p>
+              <div className="type__container-inside">
+              {
+              pokemon?.types.map((infoType, index) => (
+                <div key={infoType.type.name} style={{ backgroundColor: getBackground(index, pokemon) }} className="type__box">
+                  <p  style={{ color: getColor(index, pokemon) }} className="type__name">{infoType.type.name.toUpperCase()}</p>
+                </div>
+                ))
+              }
               </div>
-              ))
-            }
+            </div>
+            <div className="type__container two">
+              <p className="type__legend"> Habilities </p>
+              <div className="type__container-inside">
+              {
+              pokemon?.abilities?.map((infoType) => (
+                <div  className="type__box">
+                  <p key={infoType.ability.name} className="type__name">{infoType.ability.name.toUpperCase()}</p>
+                </div>
+                ))
+              }
+              </div>
             </div>
           </div>
-          <div className="type__container two">
-            <p className="type__legend"> Habilities </p>
-            <div className="type__container-inside">
-            {
-            pokemon?.abilities?.map((infoType) => (
-              <div  className="type__box">
-                <p key={infoType.ability.name} className="type__name">{infoType.ability.name.toUpperCase()}</p>
-              </div>
-              ))
-            }
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
+      </main> {/* ACABA LA PRIMERA SECCION */}
+
+      <article style={{ backgroundColor: getBackground(0, pokemon)}} className="poke-info__container">
+        <div className="poke-info__img">
+          <h1 style={{ color: getColor(0, pokemon) }} className="poke-info__name"> 
+            <Typewriter
+                                  options={{
+                                      strings: [  `stats`, 
+                                                  ],
+                                      autoStart: true,
+                                      loop: true,
+                                  }}
+                              />
+          </h1>
+        </div>
+      </article>
+
+      <article style={{ backgroundColor: getBackground(0, pokemon)}} className="poke-info__container">
+        <div className="poke-info__img">
+          <h1 style={{ color: getColor(0, pokemon) }} className="poke-info__name"> 
+            <Typewriter
+                                  options={{
+                                      strings: [  `movements`, 
+                                                  ],
+                                      autoStart: true,
+                                      loop: true,
+                                  }}
+                              />
+          </h1>
+          <div className="row">
+              {
+              pokemon?.moves?.map((infoMove) => (
+                <div  className="type__box">
+                  <p key={infoMove.move.name} className="type__name">{infoMove.move.name.toUpperCase()}</p>
+                </div>
+                ))
+              }
+              </div>
+        </div>
+      </article>
+
+    </section>
   )
 }
 
