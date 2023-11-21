96afbd6645f1f52395b7f5372ba187510ba13ad7
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-11-21 11:58:29 -0700

    initializing dark mode

---
+++ src/components/DarkMode/DarkMode.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Sun from "./Sun.svg";
+import Moon from "./Moon.svg";
+import "./DarkMode.css";
+
+const DarkMode = () => {
+    return (
+        <div className='dark_mode'>
+            <input
+                className='dark_mode_input'
+                type='checkbox'
+                id='darkmode-toggle'
+            />
+            <label className='dark_mode_label' for='darkmode-toggle'>
+                <img className="svg sun" src={Sun} alt="Sun" />
+                <img className="svg moon" src={Moon} alt="Moon" />
+            </label>
+        </div>
+    );
+};
+
+export default DarkMode;
